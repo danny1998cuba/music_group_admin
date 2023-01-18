33d81042fd0e8f895c9fb429e1de702e8046dc3d
Daniel González Cuétara <danny.glezcuet98@gmail.com>
2023-01-17 22:17:55 -0500

    Gain model

--- models/Gain.js
+++ models/Gain.js
@@ -5,34 +5,61 @@ const gainSchema = new Schema({
         type: Date,
         required: true
     },
-    currecies: [
-        {
-            currecy: {
-                type: String,
-                required: true
-            },
-            amount: {
-                type: Number,
-                required: true
-            },
-            change: {
-                type: Number
-            }
-        }
-    ],
-    total_amount: Number,
-    found: {
+    currecies: [{
+        _id: false,
+        currency: {
+            type: String,
+            required: true
+        },
         amount: {
             type: Number,
             required: true
         },
-        percent: {
-            type: Number,
-            required: true
+        change: {
+            type: Number
         }
+    }],
+    founding: {
+        type: Number,
+        required: true,
+        default: 0
     }
 }, {
     versionKey: false,
+    toObject: { virtuals: true },
+    toJSON: { virtuals: true },
+    id: false
 })
+
+/**
+ * Virtuals
+ */
+gainSchema.virtual('full_amount').get(function () {
+    if (this.currecies) {
+        let totalAmount = 0
+
+        this.currecies
+            .filter(e => e.change)
+            .forEach(curr => {
+                totalAmount += (curr.amount * curr.change)
+            })
+
+        return totalAmount
+    } else {
+        return 0
+    }
+})
+
+gainSchema.virtual('founding_percentage').get(function () {
+    return ((this.founding / this.full_amount) * 100).toFixed(2)
+})
+
+
+/**
+ *  Methods
+ */
+gainSchema.methods.totalAmount = function () {
+    return this.full_amount - this.founding
+}
 
 module.exports = model('Gain', gainSchema)
